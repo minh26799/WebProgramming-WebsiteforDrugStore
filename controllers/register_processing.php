ab38a573a5f726adb629e51419e33a28ffb3c5f5
KaNology <an.le091000@gmail.com>
2021-11-14 03:51:26 +0700

    Update register_processing.php

--- controllers/register_processing.php
+++ controllers/register_processing.php
@@ -2,7 +2,7 @@
     include_once ('../views/config.php');
     include_once('../models/Users.php');
     $newUser = new Users();
-    $newUser->createConnection($ServerName, $Username, $Password, $dbname);
+    $newUser->createConnection($servername, $username, $password, $dbname);
     if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstname'])&& isset($_POST['lastname'])) {
         $newUser->register($_POST);
     } else {
