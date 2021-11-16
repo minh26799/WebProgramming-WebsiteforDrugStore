193fe277b6aaa87284a56a223b61836dc2bd31c0
minhdang2803 <58287394+minhdang2803@users.noreply.github.com>
2021-11-16 18:03:16 +0700

    Nothing change

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -1,13 +1,9 @@
 <?php
 
 class LoginController {
-
-
-        public function view() {
-
-            $result = file_get_contents('http://'.$_SERVER['SERVER_NAME'].'/WebProgramming-WebsiteforDrugStore/views/login.php', false);
-            return $result;
-        }
-
+    public function view() {
+        $result = file_get_contents('http://'.$_SERVER['SERVER_NAME'].'/WebProgramming-WebsiteforDrugStore/views/login.php', false);
+        return $result;
+    }
 }
 ?>
