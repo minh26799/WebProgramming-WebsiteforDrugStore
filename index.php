63b7993c1b51dfb41d99bb483e895f791b213306
minhdang2803 <58287394+minhdang2803@users.noreply.github.com>
2021-11-16 18:00:05 +0700

    Add editProfile's  controller

--- index.php
+++ index.php
@@ -1,43 +1,48 @@
 <?php
 session_start();
-	require_once('router.php');
-	include_once('controllers/login.controller.php');
-	include_once('controllers/register.controller.php');
-	include_once('controllers/productDetail.controller.php');
-	include_once('controllers/home.controller.php');
-	include_once('controllers/cart.controller.php');
-	include_once('controllers/profile.controller.php');
+require_once('router.php');
+include_once('controllers/login.controller.php');
+include_once('controllers/register.controller.php');
+include_once('controllers/productDetail.controller.php');
+include_once('controllers/home.controller.php');
+include_once('controllers/cart.controller.php');
+include_once('controllers/profile.controller.php');
+include_once('controllers/editProfile.controller.php');
 
-	$router = new Router();
-	$router->addRoute('/home', function($url){
-		$home = new HomeController();
-		print_r($home->view());
-	});
+$router = new Router();
+$router->addRoute('/home', function ($url) {
+	$home = new HomeController();
+	print_r($home->view());
+});
 
-	$router->addRoute('/login', function($url){
-		$login = new LoginController();
-		print_r($login->view());
-	});
+$router->addRoute('/login', function ($url) {
+	$login = new LoginController();
+	print_r($login->view());
+});
 
-	$router->addRoute('/register', function($url){
-		$register = new RegisterController();
-		print_r($register->view());
-	});
+$router->addRoute('/register', function ($url) {
+	$register = new RegisterController();
+	print_r($register->view());
+});
 
-	$router->addRoute('/product_detail', function($url){
-		$productdetail = new ProductDetailController();
-		print_r($productdetail->view($_GET['id']));
-	});
+$router->addRoute('/editProfile', function ($url) {
+	$editProfile = new EditProfileController();
+	print_r($editProfile->view($_SESSION['id']));
+});
 
-	$router->addRoute('/cart', function($url){
-		$cart = new CartController();
-		print_r($cart->view($_GET['userid']));
-	});
+$router->addRoute('/product_detail', function ($url) {
+	$productdetail = new ProductDetailController();
+	print_r($productdetail->view($_GET['id']));
+});
 
-	$router->addRoute('/profile', function($url){
-		$profile = new ProfileController();
-		print_r($profile->view($_GET['userid']));
-	});
+$router->addRoute('/cart', function ($url) {
+	$cart = new CartController();
+	print_r($cart->view($_GET['userid']));
+});
 
-	$router->run();
-?>
+$router->addRoute('/profile', function ($url) {
+	$profile = new ProfileController();
+	print_r($profile->view($_GET['userid']));
+});
+
+$router->run();
